0f7a474ac72beb94860c0b4b7a711865f287b89f
saga_david <saga.k.david@gmail.com>
2022-06-02 00:51:31 +0200

    test github

---
+++ draft.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+// simulate the dynamic of several bodies
+
+class Body {
+    // describes one body 
+    public:
+    float mass;
+    int x, y; // make up the coordinates 
+
+};
