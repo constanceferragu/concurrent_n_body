e11e512809778c9e5b0fb7d8a45c9177391991c3
Sigurjon Agustsson <sigurjon.agustsson@bugatti.polytechnique.fr>
2022-06-02 00:42:38 +0200

    generate random bodies

--- n_body.cpp
+++ n_body.cpp
@@ -43,6 +43,27 @@ public:
 Body::~Body() {
 }
 
+std::vector<Body> generate_random_bodies(int num_bodies){
+    // For the moment, these are the boundaries:
+    //      mass is between 0 and 100
+    //      x,y coordinates are between 0 and 1
+    //      velocities are between 0 and 10 
+    std::vector<Body> bodies;
+    double m,x,y,v_x, v_y;
+    Body rand_body; 
+
+    for (int i=0; i<num_bodies; i++){
+        m = 100 * (rand() / double(RAND_MAX)) ;
+        x = (rand() / double(RAND_MAX)) ;
+        y = (rand() / double(RAND_MAX)) ;
+        v_x = 10 * (rand() / double(RAND_MAX)) ;
+        v_y = 10 * (rand() / double(RAND_MAX)) ;
+        rand_body = Body(x,y,m, v_x,v_y); 
+        bodies.push_back(rand_body); 
+    }
+    return bodies; 
+}
+
 double Body::get_force(Body* b_1, Body* b_2){
     double dist = pow( b_1->x - b_2->x , 2) + pow(b_1->y - b_2->y , 2);
     return (GRAVITY * b_1->mass * b_2->mass)/dist;
