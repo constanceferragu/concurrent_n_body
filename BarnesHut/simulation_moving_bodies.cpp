a3fd4298d6c95ed931024c03a5ddd8af8e4929f3
constanceferragu <72493914+constanceferragu@users.noreply.github.com>
2022-06-17 11:46:08 +0200

    update simulation

--- BarnesHut/simulation_moving_bodies.cpp
+++ BarnesHut/simulation_moving_bodies.cpp
@@ -1,85 +1,53 @@
-#include "barnes_hut_tee.cpp"
-#define dt 1. // Let's say that dt, our time step, is 1 second
-#define theta 0.5 // theta is 0.5 by default 
-#define max_time 3. // how long we want the simulation to last 
+#include "barnes_hut_tree.cpp"
+ #define dt 1. // Let's say that dt, our time step, is 1 second
+ #define theta 0.5 // theta is 0.5 by default 
+ #define max_time 3. // how long we want the simulation to last 
 
-int main(){
-    int N = 4; // number of bodies
-    Body** body_pointers = generate_random_bodies_pointers(N); 
-    Body *B_i; 
+ int main(){
+     Body *b_i, *b_j;
+     // number of bodies 
+     int N = 8; 
 
-    double max_dist = 0.;
-    for (int i =0; i<N; i++){
-        if (dist_to_center(body_pointers[i])>max_dist){
-            max_dist = dist_to_center(body_pointers[i]);
-        }
-    }
-    std::cout<<"max distance is: "<<max_dist<<std::endl; 
+     // create tree 
+     Body* b1 = new Body(0.1, 0.3, 1, 0, 0);
+     Body* b2 = new Body(0.1, 0.4, 2, 0, 0);
+     Body* b3 = new Body(0.6, 0.3, 3, 0, 0);
+     Body* b4 = new Body(0.6, 0.9, 4, 0, 0);
+     Body* b5 = new Body(0, 0.5, 5, 0, 0);
+     Body* b6 = new Body(0.9, 0.9, 6, 0, 0);
+     Body* b7 = new Body(0.55, 0.1, 7, 0, 0);
+     Body* b8 = new Body(0.93, 0.44, 8, 0, 0);
+     std::vector<Body*> bodies{b1, b2, b3, b4, b5, b6, b7, b8};
+     double box_dim[4] = {0,0,1,1};
+     BH_Tree* tree = new BH_Tree(box_dim, bodies);
+     tree->print_tree();
 
-    double normalise_val = 1.1* max_dist; 
+     // force matrices 
+     double force_matrix_x[N]; 
+     double force_matrix_y[N]; 
 
-    double **force_matrix_x = new double* [N];
-    double **force_matrix_y = new double* [N];
-    for (int i = 0; i<N; i++){
-        force_matrix_x[i] = new double[N];
-        force_matrix_y[i] = new double[N];
-    }
+     double time  = 0;
 
-    visualise_bodies(body_pointers, normalise_val, N); 
+     while (time <= max_time){
 
-    double time = 0;
+         // compute forces 
+         for (int i = 0; i<N; i++){
+             b_i = bodies[i]; 
+             // compute force on this body 
+             double F_x, F_y;
+             tree->compute_force_on_b(b_i, theta, F_x, F_y);
 
-    while (time<DAY){
+             force_matrix_x[i] = F_x;
+             force_matrix_y[i] = F_y;
+         }
 
-        // compute forces 
-        for (int i = 0; i<N; i++){
-            b_i = bodies[i]; 
-            // compute force on this body 
-            double F_x, F_y;
-            tree->compute_force_on_b(b_i, theta, F_x, F_y);
+         // update (x,y) coordinates 
+         for (int i = 0; i<N; i++){
+             bodies[i]->apply_force(force_matrix_x[i], force_matrix_y[i], dt);
+         }
 
-            force_matrix_x[i] = F_x;
-            force_matrix_y[i] = F_y;
-        }
-
-        // update (x,y) coordinates 
-        for (int i = 0; i<N; i++){
-            bodies[i]->apply_force(force_matrix_x[i], force_matrix_y[i], dt);
-        }
-
-        // update time 
-        time += dt; 
-        visualise_bodies(body_pointers, normalise_val, N); 
-    }
-    #ifdef PRINT_POSITIONS
-    std::cout<<"Final positions:"<<std::endl; 
-    for (int i = 0; i<N; i++){
-        // bodies[i].print();
-        body_pointers[i]->print();
-    }
-    // visualise_bodies(bodies, normalise_val); 
-    #endif 
-    #ifdef PRINT_POSITIONS_VISUAL
-    visualise_bodies(body_pointers, normalise_val, N); 
-    #endif
-
-    #ifdef PRINT_FINAL_FORCE_MATRIX
-    std::cout<<"---force matrix x:---"<<std::endl; 
-    for (int i = 0; i < N; i++){
-        for (int j = 0; j < N; j++){
-            std::cout << force_matrix_x[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-    std::cout<<"---force matrix y:---"<<std::endl; 
-    for (int i = 0; i < N; i++){
-        for (int j = 0; j < N; j++){
-            std::cout << std::setprecision(4) << force_matrix_y[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-    std::cout<<"------"<<std::endl; 
-    #endif
-
-    return 0; 
-}
+         // update time 
+         time += dt; 
+     }
+     return 0;
+ }
